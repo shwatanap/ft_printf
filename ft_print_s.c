5fb536305646d3a4aa48c714bcf293fcd1f6a0f1
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-24 11:02:18 +0900

    :memo: null handling

--- ft_print_s.c
+++ ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 10:12:17 by shwatana          #+#    #+#             */
-/*   Updated: 2022/04/24 10:18:11 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/24 10:57:48 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int	ft_print_s(int fd, char *str)
 {
 	size_t	len;
 
+	if (str == NULL)
+	{
+		write(fd, "(null)", sizeof(char) * 6);
+		return (6);
+	}
 	len = ft_strlen(str);
 	write(fd, str, (int)len);
 	return ((int)len);
