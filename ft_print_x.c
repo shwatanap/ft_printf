e863f8a2dd922178c44339108dcc50f8130a9ecd
shou-watanabe <watanabeshou1520@gmail.com>
2022-05-12 14:38:55 +0900

    :memo: update type

--- ft_print_x.c
+++ ft_print_x.c
@@ -6,13 +6,13 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 11:06:16 by shwatana          #+#    #+#             */
-/*   Updated: 2022/05/12 01:33:44 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/05/12 02:13:54 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_put_hex(int fd, int n, t_specifier specifier);
+static void	ft_put_hex(int fd, unsigned int n, t_specifier specifier);
 static int	ft_putnbr_hex_rec(int fd, unsigned int n, t_specifier specifier);
 
 int	ft_putnbr_hex(int fd, va_list *ap, t_specifier specifier)
@@ -41,7 +41,7 @@ static int	ft_putnbr_hex_rec(int fd, unsigned int n, t_specifier specifier)
 	return (print_cnt);
 }
 
-static void	ft_put_hex(int fd, int n, t_specifier specifier)
+static void	ft_put_hex(int fd, unsigned int n, t_specifier specifier)
 {
 	char	c;
 
