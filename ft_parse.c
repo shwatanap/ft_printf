ac9282995d61305d90122b87a45edcc382e0a821
shwatanap <watanabeshou1520@gmail.com>
2022-05-16 13:14:49 +0900

    :memo: handling overflow

--- ft_parse.c
+++ ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 19:23:29 by shwatana          #+#    #+#             */
-/*   Updated: 2022/05/12 17:43:17 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/05/16 12:21:57 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,21 @@ int	ft_parse(const char **format, va_list *ap)
 
 int	ft_convert(va_list *ap, t_specifier specifier)
 {
-	int	printed_cnt;
-
-	printed_cnt = 0;
 	if (specifier == C)
-		printed_cnt += ft_print_c(STDOUT_FILENO, ap);
+		return (ft_print_c(STDOUT_FILENO, ap));
 	else if (specifier == S)
-		printed_cnt += ft_print_s(STDOUT_FILENO, ap);
+		return (ft_print_s(STDOUT_FILENO, ap));
 	else if (specifier == P)
-		printed_cnt += ft_print_p(STDOUT_FILENO, ap);
+		return (ft_print_p(STDOUT_FILENO, ap));
 	else if (specifier == D || specifier == I)
-		printed_cnt += ft_print_d(STDOUT_FILENO, ap);
+		return (ft_print_d(STDOUT_FILENO, ap));
 	else if (specifier == U)
-		printed_cnt += ft_print_u(STDOUT_FILENO, ap);
+		return (ft_print_u(STDOUT_FILENO, ap));
 	else if (specifier == SX)
-		printed_cnt += ft_print_x(STDOUT_FILENO, ap, specifier);
+		return (ft_print_x(STDOUT_FILENO, ap, specifier));
 	else if (specifier == LX)
-		printed_cnt += ft_print_x(STDOUT_FILENO, ap, specifier);
+		return (ft_print_x(STDOUT_FILENO, ap, specifier));
 	else if (specifier == PER)
-		printed_cnt += ft_print_per(STDOUT_FILENO);
-	return (printed_cnt);
+		return (ft_print_per(STDOUT_FILENO));
+	return (-1);
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,54 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 17:19:55 by shwatana          #+#    #+#             */
-/*   Updated: 2022/05/12 01:45:55 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/05/16 12:39:01 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static bool	overflow_check(int augend, int addend);
+static int	va_end_and_return(va_list *ap);
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	int		printed_cnt;
+	int		print_cnt;
 
+	if (format == NULL)
+		return (-1);
 	printed_cnt = 0;
 	va_start(ap, format);
-	if (format == NULL)
-		printed_cnt = -1;
 	while (*format)
 	{
 		if (*format == '%')
-			printed_cnt += ft_parse(&format, &ap);
-		else
 		{
-			ft_putchr_fd(*format, STDOUT_FILENO);
-			format++;
-			printed_cnt++;
+			print_cnt = ft_parse(&format, &ap);
+			if (overflow_check(printed_cnt, print_cnt))
+				return (va_end_and_return(&ap));
+			printed_cnt += print_cnt;
+			continue ;
 		}
+		ft_putchr_fd(*format, STDOUT_FILENO);
+		format++;
+		if (overflow_check(printed_cnt, 1))
+			return (va_end_and_return(&ap));
+		printed_cnt++;
 	}
 	va_end(ap);
 	return (printed_cnt);
+}
+
+static bool	overflow_check(int augend, int addend)
+{
+	if (INT_MAX - augend < addend || addend == -1)
+		return (true);
+	return (false);
+}
+
+static int	va_end_and_return(va_list *ap)
+{
+	va_end(*ap);
+	return (-1);
 }
